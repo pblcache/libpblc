1948027b9e0598a60d814220959f7ecc64305cbe
Luis Pabón <lpabon@redhat.com>
2016-01-20 16:38:22 -0500

    Initial setup of the repo

--- src/pblc.c
+++ src/pblc.c
@@ -1,2 +1,10 @@
-void hello( void ) {
-}
+/*
+ *  Copyright (c) 2016 The libpblc Authors
+ *
+ *  This file is part of libpblc
+ *
+ *   This file is licensed to you under your choice of the GNU Lesser
+ *  General Public License, version 3 or any later version (LGPLv3 or
+ *  later), or the GNU General Public License, version 2 (GPLv2), in all
+ *  cases as published by the Free Software Foundation.
+ */
--- src/pblc.h
+++ src/pblc.h
@@ -1,6 +1,16 @@
-#ifndef _HELLO_H
-#define _HELLO_H
+/*
+ *  Copyright (c) 2016 The libpblc Authors
+ *
+ *  This file is part of libpblc
+ *
+ *   This file is licensed to you under your choice of the GNU Lesser
+ *  General Public License, version 3 or any later version (LGPLv3 or
+ *  later), or the GNU General Public License, version 2 (GPLv2), in all
+ *  cases as published by the Free Software Foundation.
+ */
 
-/* Exported Header */
+#ifndef _PBLC_H
+#define _PBLC_H
+
 
 #endif
