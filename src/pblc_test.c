17804368ac95e6a333eb3c23773103cf767539f6
Luis Pabón <lpabon@redhat.com>
2016-01-21 13:20:47 -0500

    Changed to Apache since pblcache uses it

--- src/pblc_test.c
+++ src/pblc_test.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008 Google Inc.
+ * Copyright (c) 2016 The libpblc authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #include <stdarg.h>
 #include <stddef.h>
 #include <setjmp.h>
