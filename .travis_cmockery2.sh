696411995a42b5cdacd697b8bfff767fd036fbe5
Luis Pabón <lpabon@redhat.com>
2016-01-26 20:54:32 -0500

    Install msgpack from Git
    Ubuntu only has the *really* old versions.

--- .travis_cmockery2.sh
+++ .travis_cmockery2.sh
@@ -9,3 +9,12 @@ sudo make install || exit 1
 cd ..
 rm -rf cmockery2
 
+git clone -b cpp-1.3.0 https://github.com/msgpack/msgpack-c.git
+cd msgpack-c
+./bootstrap || exit 1
+./configure --prefix=/usr || exit 1
+make || exit 1
+sudo make install || exit 1
+cd ..
+rm -rf msgpack-c
+
